0ab783cbbfb63dfc65f771a98de5138f4731f520
lucasrosati <lucasrosati@hotmail.com>
2024-09-29 19:28:18 -0300

    bug fixes

--- consumidor_python/consumidor.py
+++ consumidor_python/consumidor.py
@@ -1,49 +1,56 @@
 import pika
 import sys
+import time
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-
+    # Definindo as especialidades médicas disponíveis
     especialidades = ['cardiologista', 'dermatologista', 'pediatra', 'psiquiatra', 'ginecologista',
-                      'oncologista', 'geriatra', 'endocrinologista', 'urologista', 'pneumologista']
+                      'oncologista', 'geriatra', 'endocrinologista', 'urologista', 'pneumologista', 'oftalmologista']
 
     while True:
+        # Exibindo o menu de especialidades para o usuário escolher
         print("\nEscolha uma especialidade para filtrar as mensagens:")
         for i, especialidade in enumerate(especialidades, start=1):
             print(f"{i}. {especialidade}")
         print("0. Sair")
-        
+
         escolha = input("Digite o número da especialidade desejada: ")
 
+        # Encerrando o programa caso o usuário escolha "0"
         if escolha == '0':
             print("Saindo...")
             break
-
-        if escolha.lower() == 'v':
-            continue
 
         try:
             indice_escolhido = int(escolha) - 1
             if 0 <= indice_escolhido < len(especialidades):
                 especialidade = especialidades[indice_escolhido]
                 queue_name = f"consulta_fila_{especialidade}"
-                
-                print(f"\nAguardando mensagens para a especialidade: {especialidade}.")
-                print("Pressione 'ctrl+c' para voltar ao menu")
-                
-                channel.queue_declare(queue=queue_name, durable=True)
 
-                def callback(ch, method, properties, body):
-                    print(f"Consulta agendada com sucesso! Detalhes: {body.decode()}")
+                while True:
+                    try:
+                        # Conectando ao RabbitMQ
+                        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+                        channel = connection.channel()
 
-                channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+                        # Declarando a fila e aguardando as mensagens
+                        print(f"\nAguardando mensagens para a especialidade: {especialidade}.")
+                        print("Pressione 'ctrl+c' para voltar ao menu")
 
-                try:
-                    channel.start_consuming()
-                except KeyboardInterrupt:
-                    print("\nParando o consumo de mensagens...")
-                    continue
+                        channel.queue_declare(queue=queue_name, durable=True)
+
+                        def callback(ch, method, properties, body):
+                            print(f"Consulta agendada com sucesso! Detalhes: {body.decode()}")
+
+                        channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+                        channel.start_consuming()
+
+                    except pika.exceptions.AMQPConnectionError:
+                        print("Conexão perdida. Tentando reconectar em 5 segundos...")
+                        time.sleep(5)
+                    except KeyboardInterrupt:
+                        print("\nParando o consumo de mensagens...")
+                        break
             else:
                 print("Escolha inválida. Tente novamente.")
         except ValueError:
